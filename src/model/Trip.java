b75c5efafb674fc43696c5e2a396a8ddedffb2fb
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-03-05 13:02:41 -0800

    Updated Trip model to match the new database schema

--- src/model/Trip.java
+++ src/model/Trip.java
@@ -5,6 +5,9 @@ package model;
  * @author Max Sainsbury
  */
 public class Trip {
+    private int totalTime;
+    private int layoverDuration;
+    private double totalPrice;
     private int flightId;
     private int promoId;
     private int tripId;
@@ -16,8 +19,8 @@ public class Trip {
      * @param promoId
      * @param tripId 
      */
-    public Trip (int flightId, int promoId, int tripId) {
-        this(flightId, promoId);
+    public Trip (int totalTime, int layoverDuration, double totalPrice, int flightId, int promoId, int tripId) {
+        this(totalTime, layoverDuration, totalPrice, flightId, promoId);
         this.tripId = tripId;
     }
     
@@ -27,10 +30,39 @@ public class Trip {
      * @param flightId
      * @param promoId 
      */
-    public Trip(int flightId, int promoId) {
+    public Trip(int totalTime, int layoverDuration, double totalPrice, int flightId, int promoId) {
+        this.totalTime = totalTime;
+        this.layoverDuration = layoverDuration;
+        this.totalPrice = totalPrice;
         this.flightId = flightId;
         this.promoId = promoId;
     }
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public int getLayoverDuration() {
+        return layoverDuration;
+    }
+
+    public void setLayoverDuration(int layoverDuration) {
+        this.layoverDuration = layoverDuration;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    
 
     public int getFlightId() {
         return flightId;
