bb1afdfbc8c122d9ed7ea48783e94f7c7fa414ac
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-03-08 14:36:20 -0800

    Created payment class to match the new database schema

---
+++ src/model/Payment.java
@@ -0,0 +1,99 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Max Sainsbury
+ */
+public class Payment {
+    private int paymentId;
+    private int bookingId;
+    private int employeeId;
+    private LocalDate paymentDate;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    
+    public Payment(int bookingId, int employeeId, double amount, String paymentMethod, String status, String paymentDate, int paymentId) {
+        this.paymentId = paymentId;
+        this.bookingId = bookingId;
+        this.employeeId = employeeId;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.paymentDate = LocalDate.parse(paymentDate, formatter);
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.status = status;
+    }
+    
+    public Payment(int bookingId, int employeeId, double amount, String paymentMethod, String status, int paymentYear, int paymentMonth, int paymentDay) {
+        this.bookingId = bookingId;
+        this.employeeId = employeeId;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.status = status;
+        this.paymentDate = LocalDate.of(paymentYear, paymentMonth, paymentDay);
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
+
+
