0bc9145f754fbec40091860a83ea1ec213270773
Goen Choi <goyun6127@gmail.com>
2025-03-18 15:19:44 -0700

    Removed mysql-connector from library.

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -108,11 +108,9 @@ public class EmployeeDAO {
      */
     public boolean deleteEmployee(int employeeId) {
         String query = """
-                      DELETE FROM user
-                      WHERE user_id = ( SELECT user_id
-                                        FROM (SELECT employee_id
-                                              FROM user INNER JOIN employee USING (user_id)) as temp
-                                        WHERE employee_id = ?)                      
+                      DELETE u
+                      FROM user u INNER JOIN employee e USING (user_id)
+                      WHERE employee_id = ?                     
                       """;
        try(Connection connection = DBConnection.getConnection();
            PreparedStatement preparedStatement = connection.prepareStatement(query)){
