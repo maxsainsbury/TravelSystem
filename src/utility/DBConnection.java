a5e066c6377133c10a18c86b3c5c360a55162d93
Max Sainsbury <maxwell.sainsbury@gmail.com>
2025-03-22 23:00:28 -0700

    removed database password

--- src/utility/DBConnection.java
+++ src/utility/DBConnection.java
@@ -11,7 +11,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/travelsystemdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
